ebe59405ccebcb72ac9cfbabf51679f9ec6bb7a8
michael1303 <88162384+michael1303@users.noreply.github.com>
2025-05-07 10:51:01 -0400

    first commit

---
+++ webpack.common.js
@@ -0,0 +1,27 @@
+// webpack.common.js
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+    // filename will be overridden per‑env
+  },
+  module: {
+    rules: [
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({ template: "./src/index.html" }),
+  ],
+};
